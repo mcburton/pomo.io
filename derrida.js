eab6685263464c9b24cc10d4686fbe8c1274bb76
mcburton <mcburton@gmail.com>
2013-10-24 11:46:27 -0400

    adding function

--- derrida.js
+++ derrida.js
@@ -1,1 +1,6 @@
 // derrida ascii art
+
+
+function da24() {
+  window.document.getElementsByTagName('h1')[0].textContent.split('').forEach(function(x){ window.document.getElementsByTagName('h1')[0].insertAdjacentText("afterEnd",x)})
+};
