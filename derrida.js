4f3401f0fb9892453e166991fb8ca7a1d55f17ec
mcburton <mcburton@gmail.com>
2013-10-24 11:51:21 -0400

    x

--- derrida.js
+++ derrida.js
@@ -4,3 +4,9 @@
 function da24() {
   window.document.getElementsByTagName('h1')[0].textContent.split('').forEach(function(x){ window.document.getElementsByTagName('h1')[0].insertAdjacentText("afterEnd",x)})
 };
+
+function x(x) {
+   for (i=3; i < x; i++) {
+      da24();
+   };
+};
